654f08c3d790cdf2ede4b00f1e644bb10fb4b75c
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2025-09-12 18:16:43 -0500

    Se agregan los nombres del equipo al proyecto básico de Next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,13 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
+            Ana Isabel Patiño Osorio
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Jonathan Andrés Granda Orrego.
+          </li>
+          <li className="tracking-[-.01em]">
+            Cristian Daniel Muñoz Botero 
           </li>
         </ol>
 
